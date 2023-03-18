18efb876389f41ad9a6d2d9adc423e91278df5a8
Tyler Rudman <tyler.rudman@gmail.com>
2023-03-18 11:12:06 -0400

    edit syntax in webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -32,7 +32,7 @@ module.exports = () => {
         fingerprints: false,
         inject: true,
         name: "Just Another Text Editor",
-        short_name: "Jate",
+        short_name: "JATE",
         description: "Text Editor",
         background_color: "#225ca3",
         theme_color: "#225ca3",
